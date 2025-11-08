fdcae21e5bca67946eead27a44d4786fef5e63e9
supichaprom-cell <supicha.prom@bumail.net>
2025-11-08 14:39:18 +0700

    daily_vocab_api

--- api/main.py
+++ api/main.py
@@ -1,20 +1,32 @@
 from fastapi import FastAPI
-from fastapi import HTTPException
 from app.schemas import WordResponse
-from app.routers import words
-
-
+from fastapi import HTTPException
+from app.routers import words, practice
+from app.database import Base, engine
+from fastapi.middleware.cors import CORSMiddleware
+ 
+Base.metadata.create_all(bind=engine)
+ 
 # Initialize FastAPI app
 app = FastAPI(
     title="Vocabulary Practice API",
     version="1.0.0",
     description="API for vocabulary practice and learning"
 )
-
+ 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+ 
+ 
 app.include_router(words.router, prefix="/api", tags=["words"])
+app.include_router(practice.router, prefix='/api', tags=["practice"])
 
  
-
 @app.get("/")
 def read_root():
     return {
